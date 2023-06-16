ed218c100b97b195829f83e34073b6ba05be9b0b
Alireza Mahmoudian <mahmoudian@aut.ac.ir>
2023-06-17 00:19:54 +0330

    we have a height-awate system now

--- ST3.v
+++ ST3.v
@@ -2,54 +2,55 @@ Require Import List.
 Require Import lang.
 Import List.ListNotations.
 
-Reserved Notation "G ⇒3 D" (no associativity, at level 61). (* 21d2 *)
+Definition max := PeanoNat.Nat.max.
 
-Inductive ST3 : sequent -> Prop :=
-  | TId:     forall (p : atom) G, ^x_p :: G ⇒3 ^x_p
-  | TRT:     forall G, G ⇒3 ⊤
-  | TLF:     forall G D, ⊥ :: G ⇒3 D
-  | TLw:     forall G D P, G ⇒3 D -> G ++ P ⇒3 D
-  | TRw:     forall G a, G ⇒3 None -> G ⇒3 a
-  | TLa:    forall G D (a b : form), a::b::G ⇒3 D -> (a ∧ b)::G ⇒3 D
-  | TRa:     forall G (a b : form), G ⇒3 a -> G ⇒3 b -> G ⇒3 (a ∧ b)
-  | TLo:     forall G D (a b : form), a::G ⇒3 D -> b::G ⇒3 D -> (a ∨ b)::G ⇒3 D
-  | TRo1:    forall G (a b : form), G ⇒3 a -> G ⇒3 (a ∨ b)
-  | TRo2:    forall G (a b : form), G ⇒3 b -> G ⇒3 (a ∨ b)
-  | TLi:     forall G D (a b : form), ∇(a ⊃ b)::G ⇒3 a -> ∇(a ⊃ b)::b::G ⇒3 D -> ∇(a ⊃ b)::G ⇒3 D
-  | TRi:     forall G (a b : form), a::(∇l G) ⇒3 b -> G ⇒3 (a ⊃ b)
-  | TN:      forall G a, G ⇒3 Some a -> ∇l G ⇒3 Some (∇ a)
-  | TEx:    forall G P D a b, G++a::b::P ⇒3 D -> G++b::a::P ⇒3 D
-where "G ⇒3 D" := (ST3 (Seq G D)).
+Reserved Notation "G '⇒3' D '⨡' n " (no associativity, at level 61). (* 21d2, 2a21 *)
 
-Notation "⇒3 a" := ([] ⇒3 a) (no associativity, at level 62).
+Inductive ST3 : nat -> sequent -> Prop :=
+  | TId:     forall (p : atom) G, ^x_p :: G ⇒3 ^x_p ⨡ 0
+  | TRT:     forall G, G ⇒3 ⊤ ⨡ 0
+  | TLF:     forall G D, ⊥ :: G ⇒3 D ⨡ 0
+  | TLw:     forall G D P n, G ⇒3 D ⨡ n -> G ++ P ⇒3 D ⨡ S n
+  | TRw:     forall G a n, G ⇒3 None ⨡ n -> G ⇒3 a ⨡ S n
+  | TLa:    forall G D (a b : form) n, a::b::G ⇒3 D ⨡ n -> (a ∧ b)::G ⇒3 D ⨡ S n
+  | TRa:     forall G (a b : form) n m, G ⇒3 a ⨡ n -> G ⇒3 b ⨡ m -> G ⇒3 (a ∧ b) ⨡ S (max n m)
+  | TLo:     forall G D (a b : form) n m, a::G ⇒3 D ⨡ n -> b::G ⇒3 D ⨡ m -> (a ∨ b)::G ⇒3 D ⨡ S (max n m)
+  | TRo1:    forall G (a b : form) n, G ⇒3 a ⨡ n -> G ⇒3 (a ∨ b) ⨡ S n
+  | TRo2:    forall G (a b : form) n, G ⇒3 b ⨡ n -> G ⇒3 (a ∨ b) ⨡ S n
+  | TLi:     forall G D (a b : form) n m, ∇(a ⊃ b)::G ⇒3 a ⨡ n -> ∇(a ⊃ b)::b::G ⇒3 D ⨡ m -> ∇(a ⊃ b)::G ⇒3 D ⨡ S (max n m)
+  | TRi:     forall G (a b : form) n, a::(∇l G) ⇒3 b ⨡ n -> G ⇒3 (a ⊃ b) ⨡ S n
+  | TN:      forall G a n, G ⇒3 Some a ⨡ n -> ∇l G ⇒3 Some (∇ a) ⨡ S n
+  | TEx:    forall G P D a b n, G++a::b::P ⇒3 D ⨡ n -> G++b::a::P ⇒3 D ⨡ S n
+where "G ⇒3 D ⨡ n" := (ST3 n (Seq G D)).
+
+Notation "⇒3 a ⨡ n" := ([] ⇒3 a ⨡ n) (no associativity, at level 62).
+
+Reserved Notation "G ⇒3+ D ⨡ n" (no associativity, at level 61). (* 21d2, 2a21 *)
+
+Inductive ST3p : nat -> sequent -> Prop :=
+| PTId:     forall (p : atom) G, ^x_p :: G ⇒3+ ^x_p ⨡ 0
+| PTRT:     forall G, G ⇒3+ ⊤ ⨡ 0
+| PTLF:     forall G D, ⊥ :: G ⇒3+ D ⨡ 0
+| PTLw:     forall G D P n, G ⇒3+ D ⨡ n -> G ++ P ⇒3+ D ⨡ S n
+| PTRw:     forall G a n, G ⇒3+ None ⨡ n -> G ⇒3+ a ⨡ S n
+| PTLa:    forall G D (a b : form) n, a::b::G ⇒3+ D ⨡ n-> (a ∧ b)::G ⇒3+ D ⨡ S n
+| PTRa:     forall G (a b : form) n m, G ⇒3+ a ⨡ n -> G ⇒3+ b ⨡ m-> G ⇒3+ (a ∧ b) ⨡ S (max n m)
+| PTLo:     forall G D (a b : form) n m, a::G ⇒3+ D ⨡ n -> b::G ⇒3+ D ⨡ m -> (a ∨ b)::G ⇒3+ D ⨡ S (max n m)
+| PTRo1:    forall G (a b : form) n, G ⇒3+ a ⨡ n -> G ⇒3+ (a ∨ b) ⨡ S n
+| PTRo2:    forall G (a b : form) n, G ⇒3+ b ⨡ n -> G ⇒3+ (a ∨ b) ⨡ S n
+| PTLi:     forall G D (a b : form) n m, ∇(a ⊃ b)::G ⇒3+ a ⨡ n -> ∇(a ⊃ b)::b::G ⇒3+ D ⨡ m -> ∇(a ⊃ b)::G ⇒3+ D ⨡ S (max n m)
+| PTRi:     forall G (a b : form) n, a::(∇l G) ⇒3+ b ⨡ n -> G ⇒3+ (a ⊃ b) ⨡ S n
+| PTN:      forall G a n, G ⇒3+ Some a ⨡ n -> ∇l G ⇒3+ Some (∇ a) ⨡ S n
+| PTEx:    forall G P D a b n, G++a::b::P ⇒3+ D ⨡ n -> G++b::a::P ⇒3+ D ⨡ S n
+| Cut:    forall (a : form) G P D n m, G ⇒3+ a ⨡ n -> a::P ⇒3+ D ⨡ m -> G++P ⇒3+ D ⨡ S (max n m)
+where "G ⇒3+ D ⨡ n" := (ST3p n (Seq G D)).
+
+Notation "⇒3+ a ⨡ n" := ([] ⇒3+ a ⨡ n) (no associativity, at level 62).
 
 
-Reserved Notation "G ⇒3+ D" (no associativity, at level 61). (* 21d2 *)
-
-Inductive ST3p : sequent -> Prop :=
-| PTId:     forall (p : atom) G, ^x_p :: G ⇒3+ ^x_p
-| PTRT:     forall G, G ⇒3+ ⊤
-| PTLF:     forall G D, ⊥ :: G ⇒3+ D
-| PTLw:     forall G D P, G ⇒3+ D -> G ++ P ⇒3+ D
-| PTRw:     forall G a, G ⇒3+ None -> G ⇒3+ a
-| PTLa:    forall G D (a b : form), a::b::G ⇒3+ D -> (a ∧ b)::G ⇒3+ D
-| PTRa:     forall G (a b : form), G ⇒3+ a -> G ⇒3+ b -> G ⇒3+ (a ∧ b)
-| PTLo:     forall G D (a b : form), a::G ⇒3+ D -> b::G ⇒3+ D -> (a ∨ b)::G ⇒3+ D
-| PTRo1:    forall G (a b : form), G ⇒3+ a -> G ⇒3+ (a ∨ b)
-| PTRo2:    forall G (a b : form), G ⇒3+ b -> G ⇒3+ (a ∨ b)
-| PTLi:     forall G D (a b : form), ∇(a ⊃ b)::G ⇒3+ a -> ∇(a ⊃ b)::b::G ⇒3+ D -> ∇(a ⊃ b)::G ⇒3+ D
-| PTRi:     forall G (a b : form), a::(∇l G) ⇒3+ b -> G ⇒3+ (a ⊃ b)
-| PTN:      forall G a, G ⇒3+ Some a -> ∇l G ⇒3+ Some (∇ a)
-| PTEx:    forall G P D a b, G++a::b::P ⇒3+ D -> G++b::a::P ⇒3+ D
-| Cut:    forall (a : form) G P D, G ⇒3+ a -> a::P ⇒3+ D -> G++P ⇒3+ D
-where "G ⇒3+ D" := (ST3p (Seq G D)).
-
-Notation "⇒3+ a" := ([] ⇒3+ a) (no associativity, at level 62).
-
-
-Lemma ST3_ST3p : forall {s : sequent}, ST3 s -> ST3p s.
+Lemma ST3_ST3p : forall n {s : sequent}, ST3 n s -> ST3p n s.
 Proof.
-  intros s H. induction H.
+  intros n s H. induction H.
     - apply PTId.
     - apply PTRT.
     - apply PTLF.
@@ -72,4 +73,4 @@ Proof.
     - apply PTEx. apply IHST3.
 Defined.
 
-Coercion ST3_ST3p : ST3 >-> ST3p.
+Coercion ST3_ST3p : ST3 >-> ST3p. 
